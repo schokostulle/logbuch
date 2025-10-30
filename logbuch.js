1f6f450b60869faf20ae9be2cd5a88fcd32ae6cc
krake <schokostulle.777@gmail.com>
2025-10-30 11:30:31 +0100

    Aktualisieren von logbuch.js

--- logbuch.js
+++ logbuch.js
@@ -5,7 +5,7 @@
 
 // --- Supabase Setup ---
 const SUPABASE_URL = "https://bbeczprdumbeqcutqopr.supabase.co";
-const SUPABASE_KEY = "anon_key"; // ⚠️ Anpassen bei Bedarf
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJiZWN6cHJkdW1iZXFjdXRxb3ByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NjMzMTgsImV4cCI6MjA3NzMzOTMxOH0.j2DiRK_40cSiFOM8KdA9DzjLklC9hXH_Es6mHPOvPQk"; // ⚠️ Anpassen bei Bedarf
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // =============================================================
