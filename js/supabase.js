7fb8e2650f14da135acb9d4a001ea0528166d829
krake <schokostulle.777@gmail.com>
2025-12-03 17:18:36 +0100

    Aktualisieren von supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -6,8 +6,8 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 // Supabase Konfiguration
-const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+const SUPABASE_URL = "https://mfoyeybfjawbzlxcoqzh.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_2WfOmGSQ0NhSmHf1yBHeaw_-JIYlNQY";
 
 // Supabase Client erzeugen
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
