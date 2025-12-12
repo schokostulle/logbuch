1abf00f2c532c57aeed722b87a3814cf0c92c93a
krake <schokostulle.777@gmail.com>
2025-12-12 14:41:21 +0100

    diplomacy.js erstellen

---
+++ diplomacy/diplomacy.js
@@ -0,0 +1,10 @@
+.diplomacy-wrapper {
+    padding: var(--space-xl);
+}
+
+.diplomacy-wrapper h2 {
+    font-family: var(--font-smallcaps);
+    font-size: var(--font-size-xl);
+    color: var(--color-brass);
+    margin: var(--space-xl) 0 var(--space-m);
+}
