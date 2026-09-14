e63a69355ff2cbcc227c10854c2d2d9b9709467f
krake <schokostulle.777@gmail.com>
2026-09-14 14:38:44 +0200

    config.js erstellen

---
+++ config.js
@@ -0,0 +1,9 @@
+// Gemeinsame Konfiguration für alle Logbuch-Module.
+// Trage hier einmalig deine Supabase-Projekt-URL und deinen "anon" Public Key ein.
+const CONFIG = {
+  supabaseUrl: 'https://xqkbbhafdhplweszyjjc.supabase.co/rest/v1/',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa2JiaGFmZGhwbHdlc3p5ampjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODAyOTQsImV4cCI6MjEwNDk1NjI5NH0.xbIBCvGBevl6bT3-VMZvNwVkZWzn4E3PqLJIE6GVsIA',
+  // Feste interne "Fake"-E-Mail für den einen Logbuch-Account (in Supabase Auth angelegt).
+  // Nach außen sieht der Nutzer nur ein Passwortfeld.
+  fixedEmail: 'logbuch@internal.local'
+};
