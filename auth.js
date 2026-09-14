399a328f05ad509675d65e0aa31ae15a011e2a4f
krake <schokostulle.777@gmail.com>
2026-09-14 15:28:03 +0200

    Aktualisieren von auth.js

--- auth.js
+++ auth.js
@@ -10,6 +10,7 @@ const Auth = (function () {
   function showApp() {
     document.getElementById('loginBox').style.display = 'none';
     document.getElementById('app').style.display = 'block';
+    if (window.Nav) Nav.render();
     if (readyCallback) readyCallback(client);
   }
 
