eff2450982d6e8e1ddfc9fc87dbe6205390ebd9c
krake <schokostulle.777@gmail.com>
2026-09-14 14:57:24 +0200

    Aktualisieren von config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ const CONFIG = {
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhxa2JiaGFmZGhwbHdlc3p5ampjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzODAyOTQsImV4cCI6MjEwNDk1NjI5NH0.xbIBCvGBevl6bT3-VMZvNwVkZWzn4E3PqLJIE6GVsIA',
   // Feste interne "Fake"-E-Mail für den einen Logbuch-Account (in Supabase Auth angelegt).
   // Nach außen sieht der Nutzer nur ein Passwortfeld.
-  fixedEmail: 'logbuch@internal.local'
+  fixedEmail: 'Krake@bullfrog.local'
 };
